f1a71f8718032361824f9eab89d525da288bee48
kunal7236 <kunakashyap277@gmail.com>
2025-08-05 03:26:37 +0530

    CORS added

--- main.py
+++ main.py
@@ -10,6 +10,15 @@ load_dotenv()
 API_KEY = os.getenv("API_KEY")
 
 app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/location")
 def get_by_location(state: str, district: str):
